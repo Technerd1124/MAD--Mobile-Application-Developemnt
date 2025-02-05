de6bcdaf0f541a90bd2aa918d479a9bcbfe3e658
Dhanshri246 <technerd1124@gmail.com>
2025-02-05 10:12:36 +0000

    Practical 9 -n twitter Login

--- Practical_10/Exercise-1/MainActivity.java
+++ Practical_10/Exercise-1/MainActivity.java
@@ -1,13 +1,10 @@
 package com.example.practical_10_ex_1;
-
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
 import  android.widget.*;
 
-
 public class MainActivity extends AppCompatActivity {
-
     public Button loginBtn;
     public  EditText user,pass;
     public TextView resultState;
@@ -15,14 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-      user = (EditText) findViewById(R.id.usertv);
-      pass = (EditText) findViewById(R.id.passtv);
+      user =  findViewById(R.id.usertv);
+      pass =  findViewById(R.id.passtv);
       loginBtn = findViewById(R.id.button);
+      resultState = findViewById(R.id.stateView);
 
+     loginBtn.setOnClickListener(new View.OnClickListener() {
 
+         @Override
+         public void onClick(View view) {
+             String  username = user.getText().toString().trim();
+             String password = pass.getText().toString().trim();
+             if (username.isEmpty() && password.isEmpty()) {
+                 resultState.setText("Login Unsuccessful: Fields cannot be empty.");
+             }else {
+                 resultState.setText("Login Successful");
+             }
 
-
+         }
+     });
 
     }
 
